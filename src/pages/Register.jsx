63166132c25cf3e3e9414ec7fd8c3b105d082b74
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-02-25 21:23:45 +0600

    add login and register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
 
 export default function Register() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+
+    }
     return (
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
+            <form className="card-body" onSubmit={handleSubmit}>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email</span>
                     </label>
-                    <input type="email" placeholder="email" className="input input-bordered" required />
+                    <input type="email" placeholder="email" name="email" className="input input-bordered" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" placeholder="password" className="input input-bordered" required />
+                    <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                     <label className="label">
                         <span >Already have an account?<Link to={'/login'} className="label-text-alt link link-hover"> Login</Link> </span>
                     </label>
